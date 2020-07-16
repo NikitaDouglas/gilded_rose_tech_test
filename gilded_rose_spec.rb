70233d69bac1b914e1e9c138de5a3f7bf126f692
Nikita <latinisnotdead07@gmail.com>
2020-07-16 11:28:18 +0100

    #update quality reduses sell_in by 1, and quality by 1 if sell_in > 0

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -8,6 +8,18 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "reduces the sell_in value by 1" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq -1
+    end
+
+    it "reduces the quality by 1, when sell-in > 0" do
+      items = [Item.new("foo", 2, 2)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 1
+    end
   end
 
 end
